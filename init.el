df5be03c65823cc178ccc776fbe9015225531085
Lori M Olson <lori@wndx.com>
2022-05-08 17:54:24 -0600

    Remove a bunch of stuff I don't use. Add my .emacs file as init-local.el

--- init.el
+++ init.el
@@ -61,14 +61,11 @@
 (require 'init-uniquify)
 (require 'init-ibuffer)
 (require 'init-flymake)
-(require 'init-eglot)
 
 (require 'init-recentf)
 (require 'init-minibuffer)
 (require 'init-hippie-expand)
-(require 'init-company)
 (require 'init-windows)
-(require 'init-sessions)
 (require 'init-mmm)
 
 (require 'init-editing-utils)
@@ -79,16 +76,12 @@
 (require 'init-git)
 (require 'init-github)
 
-(require 'init-projectile)
-
 (require 'init-compile)
 (require 'init-crontab)
 (require 'init-textile)
 (require 'init-markdown)
 (require 'init-csv)
-(require 'init-erlang)
 (require 'init-javascript)
-(require 'init-php)
 (require 'init-org)
 (require 'init-nxml)
 (require 'init-html)
@@ -96,28 +89,19 @@
 (require 'init-haml)
 (require 'init-http)
 (require 'init-python)
-(require 'init-haskell)
-(require 'init-elm)
-(require 'init-purescript)
 (require 'init-ruby)
 (require 'init-rails)
 (require 'init-sql)
-(require 'init-ocaml)
 (require 'init-j)
-(require 'init-nim)
 (require 'init-rust)
 (require 'init-toml)
 (require 'init-yaml)
-(require 'init-docker)
-(require 'init-terraform)
 (require 'init-nix)
 (maybe-require-package 'nginx-mode)
 
 (require 'init-paredit)
 (require 'init-lisp)
 (require 'init-slime)
-(require 'init-clojure)
-(require 'init-clojure-cider)
 (require 'init-common-lisp)
 
 (when *spell-check-support-enabled*
@@ -130,7 +114,6 @@
 
 ;;(require 'init-twitter)
 ;; (require 'init-mu)
-(require 'init-ledger)
 ;; Extra packages which don't require any configuration
 
 (require-package 'sudo-edit)
---
+++ lisp/init-local.el
@@ -0,0 +1,11 @@
+;; Lori's stuff
+(global-set-key "\C-x\C-z" 'save-buffers-kill-emacs)
+(global-set-key [home] 'beginning-of-line)
+(global-set-key [\S-home] 'beginning-of-buffer)
+(global-set-key [end] 'end-of-line)
+(global-set-key [\S-end] 'end-of-buffer)
+(setq default-tab-width 2)
+(set-foreground-color "medium spring green")
+(set-background-color "Black")
+(set-cursor-color "GoldenRod")
+(set-frame-font "Menlo 16" nil t)
